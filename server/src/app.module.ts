10dbebf79b43362e555249295f2cfe998ad3333b
OmarAboelnaga121 <omaraboelnaga121@gmail.com>
2025-06-18 14:06:08 +0300

    Refactor server structure: remove AppController and AppService, add Faq, Portfolio, Services, Rates, and Pricing modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { FaqModule } from './faq/faq.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { ServicesModule } from './services/services.module';
+import { RatesModule } from './rates/rates.module';
+import { PricingModule } from './pricing/pricing.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [FaqModule, PortfolioModule, ServicesModule, RatesModule, PricingModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
