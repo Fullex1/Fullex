f5319136577312d25299cabaef7f3e44d1c2bac1
OmarAboelnaga121 <omaraboelnaga121@gmail.com>
2025-06-26 14:47:00 +0300

    feat: update client configuration and enhance UI components
    - Removed the test-server service from docker-compose.yml.
    - Added a new client service in docker-compose with development environment settings.
    - Updated client Dockerfile to simplify the build process.
    - Upgraded Next.js dependency to version 15.3.4 in package.json and package-lock.json.
    - Refactored global styles in globals.css to improve UI design and added new button styles.
    - Updated layout.tsx to use the Kanit font and added a loading state with Suspense.
    - Simplified the home page structure and integrated a Navbar component for better navigation.

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Kanit } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const kanit = Kanit({
+  variable: "--font-kanit",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <Suspense fallback={<div>Loading...</div>}>
+        <body
+          className={`${kanit.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </Suspense>
+     
     </html>
   );
 }
